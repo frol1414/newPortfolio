63043d9340e310dee5d3c2d5e3f08378b8579079
frol1414 <frol1414@gmail.com>
2020-09-27 09:34:52 +0300

    fix

--- src/views/FrolovAlexander/FrolovAlexander.js
+++ src/views/FrolovAlexander/FrolovAlexander.js
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 import {Switch, Route} from 'react-router-dom';
 import MainF from "../../components/FrolovAlexander/MainF/MainF";
 import AllMyWorks from "../../components/FrolovAlexander/AllMyWorks/AllMyWorks";
-import MainHeader from "../../components/FrolovAlexander/MainHeader/MainHeader";
+// import MainHeader from "../../components/FrolovAlexander/MainHeader/MainHeader";
 import './FrolovAlexander.css';
 
 
@@ -10,7 +10,7 @@ export default class FrolovAlexander extends Component {
   render() {
     return (
       <React.Fragment>
-        <MainHeader />
+        {/*<MainHeader />*/}
         <Switch>
           <Route path="/Frolov-Alexander/all-my-works" component={AllMyWorks} />
           <Route path="/" component={MainF} />
