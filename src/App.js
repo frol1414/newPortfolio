b66fefd802aec0ee6dcae18da75708617e948a09
frol1414 <frol1414@gmail.com>
2020-10-01 08:45:22 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header/Header";
 import Main from "./pages/Main/Main";
 import About from "./pages/About/About";
 import Works from "./pages/Works/Works";
+import OneWork from "./pages/OneWork/OneWork";
 import Contacts from "./pages/Contacts/Contacts";
 
 import FrolovAlexander from "./views/FrolovAlexander/FrolovAlexander";
@@ -20,6 +21,7 @@ function App() {
         <Route path="/" component={Main} exact/>
         <Route path="/about" component={About} />
         <Route path="/works" component={Works} />
+        <Route path="/work/{id}" component={OneWork} />
         <Route path="/contacts" component={Contacts} />
       </Switch>
     </Fragment>
---
+++ src/pages/OneWork/OneWork.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './OneWork.css'
+
+const OneWork = props => {
+    return (
+        <div className="one-work">
+          <p className="page__title">One work</p>
+        </div>
+    )};
+
+export default OneWork
+
