9e37b1a0dea75046b0ce34add26172b781a63d5d
frol1414 <frol1414@gmail.com>
2020-10-07 10:14:50 +0300

    fix

--- src/store/redusers/other.js
+++ src/store/redusers/other.js
@@ -8,27 +8,16 @@ const initialState = {
     {img: '/images/photo/6.jpg', class: "blue"},
   ],
   facts: [
-    {title: 'Люблю кодить, если я не пишу, значит я ем', desc: "или сплю, или путешествую", class: 'blue'},
-    {title: 'Я постоянно что-то читаю или смотрю, чтобы узнать новое', desc: "рецепты по еде не учитываю", class: 'pink'},
-    {title: 'я умный парень', desc: "ну так говорит моя мама", class: 'yellow'},
-    {title: 'я слушая музыку, всегда везде', desc: "правда остальным тоже приходится слушать", class: 'blue'},
-    {title: 'У меня нет вредных привычек', desc: " ну кроме компьютерных игр", class: 'pink'},
-    {title: 'Никогда не давайте мне играть в Факторио', desc: "никогда, слышите, никогда", class: 'yellow'},
+    {title: 'I love to code ... if I do not write, then I am dreaming', desc: "or eating or traveling", class: 'blue'},
+    {title: 'I am constantly reading or watching to learn new things', desc: "especially food recipes", class: 'pink'},
+    {title: 'I\'m a smart guy', desc: "well my mom says so", class: 'yellow'},
+    {title: 'I am listening to music, anytime, anywhere', desc: "well, at least I'm not dancing", class: 'blue'},
+    {title: 'I have no bad habits', desc: "well, except for computer games", class: 'pink'},
+    {title: 'Never let me play Factorio or Stellaris', desc: "combinators, galaxies, oh.", class: 'yellow'},
   ]
 }
 //frontfas.com/frontfas.dev/frontfa.com/frontfa.dev/fullstacker.ru/fafront.dev
 
-//Это мое небольшое портфолио с описанием моих навыков и списком моих работ. Если будет необходимо в конце есть мои контакты.
-
-/* факты
-- Люблю кодить, если я не пишу, значит я ем...или сплю, или путешествую
-- Я постоянно что-то читаю или смотрю, чтобы узнать новое...рецепты по еде не учитываю
-- я умный парень...честно...ну так говорит моя мама
-- я слушая музыку, всегда везде...правда остальным тоже приходится слушать
-- У меня нет вредных привычек, ну кроме компьютерных игр
-- Никогда не давайте мне играть в Факторио...никогда, слышите, никогда
-
-* */
 export default function skillsReducer(state = initialState, action) {
   return state
 }
